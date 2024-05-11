1983e3eb0aa3efafd815f670eab0122bb91b5f41
NicolasPozzer <pozzernico@gmail.com>
2024-05-10 23:30:35 -0300

    publicado en github

---
+++ Persona.py
@@ -0,0 +1,9 @@
+class Persona:
+    def __init__(self, nombre=""):
+        self.nombre = nombre
+        self.amigos = []
+
+    def nombreDePersona(self, persona):
+        print(persona.nombre)
+
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from Persona import Persona
+
+# Creamos una persona
+persona = Persona()
+persona2 = Persona("pedro")
+
+persona.nombreDePersona(persona)
